322b11867b1d9bae1c92d786a69edfa2c40f5bbd
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-24 17:44:43 +0200

    Zadanie: zrobione

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function mojaFunckja()
+{
+    var element1 = 11;
+    var element2 = 9;
+    var element3 = 12;
+
+    if(element1 > 10){
+        element1 = 5;
+    }
+
+    if(element2 > 10){
+        element2 = 5;
+    }
+
+    if(element3 > 10){
+       element3 = 5;
+    }
+
+    tab = [element1, element2, element3];
+
+    console.log(tab);
+}
+
+mojaFunckja();
+
+
+
+
+
